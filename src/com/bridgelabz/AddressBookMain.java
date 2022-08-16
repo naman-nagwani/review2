b4616c429186dc6a54509c86cf87ebb59edd72c8
“naman-nagwani” <namannagwani@gmail.com>
2022-08-16 09:24:26 +0530

    Contact List added

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook();
+        Contacts contacts1 = new Contacts("Naman Nagwani", 24, 9004, "jdp",
+                "C.G.");
+        int n =0;
+        Scanner scan = new Scanner(System.in);
+        addressBook.addContact(contacts1);
+        while (n!=5){
+            System.out.println("1.Add Contact \n2.Remove Contact"
+                    + " \n3.Edit Contact \n4.Print all Contact \n5. Exit");
+            n = scan.nextInt();
+            switch (n){
+                case 1:
+                    addressBook.addNewContact();
+                    break;
+                case 2:
+                    addressBook.removeContactByName();
+                    break;
+                case 3:
+                    addressBook.editContact();
+                    break;
+                case 4:
+                    addressBook.printAllContacts();
+                    break;
+                case 5:
+                    System.out.println("Exiting");
+                    break;
+                default:
+                    System.out.println("Invalid Selection ");
+            }
+        }
+    }
+}
