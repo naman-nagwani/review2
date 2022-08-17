c0af63ea140b18f6ff1062c57a9d068bd3e9061b
“naman-nagwani” <namannagwani@gmail.com>
2022-08-17 16:11:43 +0530

    Added : option for multiple address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,40 +1,109 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookMain {
 
+    private static List<AddressBook> addressBookList = new ArrayList<>();
+
+    private static Map<String, AddressBook> map
+            = new HashMap<String, AddressBook>();
+
+    private static AddressBook addressBook;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        AddressBook addressBook = new AddressBook();
-        Contacts contacts1 = new Contacts("Naman Nagwani", 24, 9004, "jdp",
-                "C.G.");
         int n =0;
         Scanner scan = new Scanner(System.in);
-        addressBook.addContact(contacts1);
+
         while (n!=5){
-            System.out.println("1.Add Contact \n2.Remove Contact"
-                    + " \n3.Edit Contact \n4.Print all Contact \n5. Exit");
+            System.out.println("1.Create new Address book \n2.Remove Address book"
+                    + "\n3.Print all Address book \n4. Select Address book \n5. Exit");
             n = scan.nextInt();
             switch (n){
                 case 1:
-                    addressBook.addNewContact();
+                    addNewAddressBook();
                     break;
                 case 2:
-                    addressBook.removeContactByName();
+                    removeAddressBook();
                     break;
                 case 3:
-                    addressBook.editContact();
+                    showAddressBook();
                     break;
                 case 4:
-                    addressBook.printAllContacts();
+                    selectAddressBook();
                     break;
                 case 5:
-                    System.out.println("Exiting");
+                    System.out.println("Exiting....");
                     break;
                 default:
-                    System.out.println("Invalid Selection ");
+                    System.out.println("Invalid Input");
             }
         }
+
+    }
+
+    private static void selectAddressBook() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of address book to be selected");
+        String name = scan.nextLine();
+        for (Map.Entry<String, AddressBook> m : map.entrySet()) {
+            if (m.getKey().equals(name)) {
+                addressBook = m.getValue();
+                int option=0;
+                while (option!=5){
+                    System.out.println("1.Add Contact \n2.Remove Contact"
+                        + " \n3.Edit Contact \n4.Print all Contact \n5. Back to main menu");
+                option = scan.nextInt();
+                switch (option){
+                    case 1:
+                        addressBook.addNewContact();
+                        break;
+                    case 2:
+                        addressBook.removeContactByName();
+                        break;
+                    case 3:
+                        addressBook.editContact();
+                        break;
+                    case 4:
+                        addressBook.printAllContacts();
+                        break;
+                    case 5:
+                        System.out.println("Back to main menu");
+                        break;
+                    default:
+                        System.out.println("Invalid Selection ");
+                    }
+                }
+
+            }
+        }
+    }
+
+    private static void showAddressBook() {
+
+        for (Map.Entry<String, AddressBook> m : map.entrySet()){
+            System.out.println(m.getKey());
+            }
+    }
+
+    private static void removeAddressBook() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of Address Book to be removed");
+        String name = scan.nextLine();
+        for (Map.Entry<String, AddressBook> m : map.entrySet()){
+            if (m.getKey().equals(name)){
+                map.remove(name);
+            }
+        }
+    }
+
+    public static void addNewAddressBook() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of Address Book");
+        String name = scan.nextLine();
+        AddressBook addressBook = new AddressBook();
+        addressBookList.add(addressBook);
+        map.put(name,addressBook);
     }
 }
